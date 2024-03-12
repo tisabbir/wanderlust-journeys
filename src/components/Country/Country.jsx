5be46b8e8aa9b26463efb2f5a56e8048a7a42da0
TI Sabbir <tisabbir0@gmail.com>
2024-03-12 15:12:02 +0600

    country named loaded from data

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,10 @@
+const Country = ({ country }) => {
+  const { name } = country;
+  return (
+    <div>
+      <h3>Name : {name?.common} </h3>
+    </div>
+  );
+};
+
+export default Country;
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setCountries(data));
   }, []);
   return (
     <div>
-      <h3>Hello World</h3>
+      <h3>Countries : {countries.length}</h3>
+      {countries.map((country) => (
+        <Country key={country.cca3} country={country}></Country>
+      ))}
     </div>
   );
 };
